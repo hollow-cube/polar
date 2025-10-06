cef887dd4d42a5b9cd0f2b3003c06c815175db34
Gravy Boat <gravy@thatgravyboat.tech>
2025-10-06 17:25:11 -0230

    include junit platform launcher

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,6 +37,8 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation(libs.minestom)
+
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 java {
